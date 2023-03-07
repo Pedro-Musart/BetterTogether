fb64ec38ffe7b4bb05d9c6a9be12360d02d75ba4
Diego Cruz <diego0260203@gmail.com>
2023-03-07 09:19:10 -0300

    create empty login

---
+++ betterTogetherfull/app/src/main/java/com/login/bettertogether/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.login.bettertogether.ui.login
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.login.bettertogether.data.LoginDataSource
+import com.login.bettertogether.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
