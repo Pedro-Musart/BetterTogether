ea56df563aac9b92d9b7119355aa17b23ea65756
Pedro Soares <pedrowesleyfs@gmail.com>
2023-03-11 20:36:55 -0300

    Add activity home

--- app/src/main/java/br/com/bettertogether/CollaboratorSignUp.kt
+++ app/src/main/java/br/com/bettertogether/CollaboratorSignUp.kt
@@ -1,7 +1,7 @@
 package br.com.bettertogether
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class CollaboratorSignUp: AppCompatActivity() {
 
