ad278d0916d7f15f16d9123336862554c5136800
Izabelle <izabelle.sdn@gmail.com>
2023-03-11 20:01:42 -0300

    Update MainActivity.kt

--- app/src/main/java/br/com/bettertogether/MainActivity.kt
+++ app/src/main/java/br/com/bettertogether/MainActivity.kt
@@ -2,7 +2,7 @@ package br.com.bettertogether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
