883eac096e9905f70f795537d544565b8286cfef
Pedro Soares <pedrowesleyfs@gmail.com>
2023-03-11 23:59:55 -0300

    Estilizei a tela de cadastro de empresa e do usuário

--- app/src/main/java/br/com/bettertogether/SignUpActivity.kt
+++ app/src/main/java/br/com/bettertogether/SignUpActivity.kt
@@ -1,14 +1,12 @@
 package br.com.bettertogether
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        setContentView(R.layout.activity_company_signup)
     }
 }
